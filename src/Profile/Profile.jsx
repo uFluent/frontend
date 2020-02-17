0678fcac58b4280c368cfd309bb45ac738d3245e
joecooper1 <joecooper2991@gmail.com>
2020-02-17 16:09:53 +0000

    changed file path for styles

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, View, Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-import styles from "./Styles";
+import styles from "../Styles";
 
 const Profile = () => {
   const navigation = useNavigation();
