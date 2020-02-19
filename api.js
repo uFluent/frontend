56d774d95ebbd024a430b6c9373a7219f93fe296
ItchyNg <andyng06@hotmail.com>
2020-02-19 10:37:34 +0000

    added api Function for text speech and translator

---
+++ api.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Constants from "expo-constants";
+import * as Speech from "expo-speech";
+
+export const translateWord = (wordToTranslate, languageToChangeTo) => {
+  return fetch(
+    `https://api.mymemory.translated.net/get?q=${wordToTranslate}&langpair=en|${languageToChangeTo}`
+  )
+    .then(response => response.json())
+    .then(responseJson => {
+      return responseJson.matches;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
+
+export const sayWord = (word, language) => {
+  Speech.speak(word, {
+    language: language,
+    pitch: 1,
+    rate: 1
+  });
+};
